772949be7ed7ede99385cad0f40097854b322302
Jeevanandhan <76152335+Jeevanandhan01@users.noreply.github.com>
2023-10-23 22:10:10 +0530

    Add files via upload

---
+++ Char-Pattern-2.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	scanf("%c",&c);
+	char  temp = c;
+	for(int i=1;i<=5;i++)
+	{
+		
+		for(int j=1;j<=i;j++)
+		{
+			printf("%c ",temp);
+			temp-=1;
+		}
+		temp = c+i;
+		printf("\n");
+	}
+}
+
+//Output :
+//A
+//BA
+//CBA
+//DCBA
+//EDCBA
